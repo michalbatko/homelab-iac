b9e21b7e9fc577dd6e549abac9467bb7e484e450
michalbatko <mibatko@gmail.com>
2025-07-27 20:20:50 +0200

    feat: Add Cloudflare's Terraform provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,17 @@
 terraform {
   required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
     proxmox = {
       source = "bpg/proxmox"
       version = "~> 0.80"
     }
   }
+}
+
+provider "cloudflare" {
 }
 
 provider "proxmox" {
