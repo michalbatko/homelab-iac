eb730040002493028358683c8c3f1bd37985c317
michalbatko <mibatko@gmail.com>
2025-07-28 12:27:20 +0200

    fix: Use correct syntax for bpg/proxmox provider
    The original DNS module was mistakenly written using the syntax
    for the telmate/proxmox provider

--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -1,29 +1,46 @@
-resource "proxmox_lxc" "dns" {
-  target_node = "infra"
-  vmid        = 100
-  hostname    = "dns"
+resource "proxmox_virtual_environment_container" "dns" {
   description = "AdGuard Home DNS server"
-  ostemplate  = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
+  node_name   = "infra"
+  vm_id       = 100
 
-  cores        = 1
-  memory       = 1024
-  swap         = 512
+  cpu {
+    cores = 1 
+  }
+
+  memory {
+    dedicated = 1024 
+    swap      = 512 
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 16
+  }
+
+  network_interface {
+    name = "eth0"
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
+    type             = "debian"
+  }
+
+  initialization {
+    hostname = "dns"
+    ip_config {
+      ipv4 {
+        address = "192.168.100.20"
+        gateway = "192.168.100.1"
+      }
+    }
+  }
+
   unprivileged = true
-  start        = true
-
-  rootfs {
-    storage = "local-lvm"
-    size    = "16G"
-  }
-
-  network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "192.168.100.20/24"
-    gw     = "192.168.100.1"
-  }
 
   features {
     nesting = true
   }
+
+  tags = []
 }
