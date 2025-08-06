89b17f6bed83833159a32664a17a628aeed9719b
michalbatko <mibatko@gmail.com>
2025-08-06 17:37:48 +0200

    chore: Reorganize Terraform directory structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,5 @@
-module "automation" {
-  source = "./automation"
-
-  ansible_ssh_public_key = var.ansible_ssh_public_key
-}
-
-module "dns" {
-  source = "./dns"
+module "proxmox" {
+  source = "./proxmox"
 
   ansible_ssh_public_key = var.ansible_ssh_public_key
 }
