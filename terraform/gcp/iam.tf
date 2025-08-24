15ab0a6a67f39a61f349983f45d9a2df969340b8
michalbatko <mibatko@gmail.com>
2025-08-24 18:14:58 +0200

    feat: Refactor IAM to use granular role assignment

--- terraform/gcp/iam.tf
+++ terraform/gcp/iam.tf
@@ -1,16 +1,26 @@
+resource "google_project_iam_member" "my_roles" {
+  project = google_project.homelab_iac.project_id
+  role    = "roles/owner"
+  member  = "user:mibatko@gmail.com"
+}
+
 resource "google_service_account" "terraform" {
+  project      = google_project.homelab_iac.project_id
   account_id   = "terraform"
   display_name = "Terraform"
   description  = "Service account used by Terraform to manage Google Cloud Platform"
-  project      = google_project.homelab_iac.project_id
 }
 
-resource "google_project_iam_binding" "owners" {
+resource "google_project_iam_member" "terraform_roles" {
   project = google_project.homelab_iac.project_id
-  role    = "roles/owner"
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.terraform.email}"
 
-  members = [
-    "serviceAccount:terraform@homelab-iac.iam.gserviceaccount.com",
-    "user:mibatko@gmail.com",
-  ]
+  for_each = toset([
+    "roles/iam.securityAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountKeyAdmin",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/storage.admin"
+  ])
 }
