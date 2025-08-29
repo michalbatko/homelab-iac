4d46df76b461d2fb620cba711edde98d46a428a3
michalbatko <mibatko@gmail.com>
2025-08-29 22:13:31 +0200

    chore: Inline required APIs instead of using local

--- terraform/gcp/locals.tf
+++ terraform/gcp/locals.tf
@@ -1,9 +1,3 @@
 locals {
-  required_apis = toset([
-    "cloudresourcemanager.googleapis.com",
-    "iam.googleapis.com",
-    "storage.googleapis.com"
-  ])
-  
   github_repository = "michalbatko/homelab-iac"
 }
--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -4,10 +4,14 @@ resource "google_project" "homelab_iac" {
 }
 
 resource "google_project_service" "required_apis" {
-  for_each = local.required_apis
-
   project = google_project.homelab_iac.project_id
   service = each.value
+
+  for_each = toset([
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "storage.googleapis.com"
+  ])
 
   disable_on_destroy = false
 }
