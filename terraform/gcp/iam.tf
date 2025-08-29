018a1db295e4bc73c551d0f522050df2655eabb9
michalbatko <mibatko@gmail.com>
2025-08-29 22:49:08 +0200

    chore: Reorder resources for better readability

--- terraform/gcp/iam.tf
+++ terraform/gcp/iam.tf
@@ -3,38 +3,6 @@ resource "google_project_iam_member" "my_roles" {
   member = "user:mibatko@gmail.com"
 }
 
-resource "google_service_account" "terraform" {
-  account_id   = "terraform"
-  display_name = "Terraform"
-  description  = "Service account used by Terraform to manage Google Cloud Platform"
-}
-
-resource "google_project_iam_member" "terraform_roles" {
-  role   = each.value
-  member = "serviceAccount:${google_service_account.terraform.email}"
-
-  for_each = toset([
-    "roles/iam.securityAdmin",
-    "roles/iam.serviceAccountAdmin",
-    "roles/iam.serviceAccountKeyAdmin",
-    "roles/iam.workloadIdentityPoolAdmin",
-    "roles/serviceusage.serviceUsageAdmin",
-    "roles/storage.admin"
-  ])
-}
-
-resource "google_service_account" "vault" {
-  account_id   = "hc-vault"
-  display_name = "HashiCorp Vault"
-  description  = "Service account used by HashiCorp Vault to access backend on GCS storage"
-}
-
-resource "google_storage_bucket_iam_member" "vault_roles" {
-  bucket = google_storage_bucket.vault_backend.name
-  role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${google_service_account.vault.email}"
-}
-
 resource "google_iam_workload_identity_pool" "github" {
   workload_identity_pool_id = "github"
 }
@@ -59,8 +27,40 @@ resource "google_iam_workload_identity_pool_provider" "github" {
   attribute_condition = "attribute.repository == '${local.github_repository}'"
 }
 
+resource "google_service_account" "terraform" {
+  account_id   = "terraform"
+  display_name = "Terraform"
+  description  = "Service account used by Terraform to manage Google Cloud Platform"
+}
+
+resource "google_project_iam_member" "terraform_roles" {
+  role   = each.value
+  member = "serviceAccount:${google_service_account.terraform.email}"
+
+  for_each = toset([
+    "roles/iam.securityAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountKeyAdmin",
+    "roles/iam.workloadIdentityPoolAdmin",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/storage.admin"
+  ])
+}
+
 resource "google_service_account_iam_member" "terraform_workload_identity" {
   service_account_id = google_service_account.terraform.name
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/projects/${google_project.homelab_iac.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github.workload_identity_pool_id}/attribute.repository/${local.github_repository}"
+}
+
+resource "google_service_account" "vault" {
+  account_id   = "hc-vault"
+  display_name = "HashiCorp Vault"
+  description  = "Service account used by HashiCorp Vault to access backend on GCS storage"
+}
+
+resource "google_storage_bucket_iam_member" "vault_roles" {
+  bucket = google_storage_bucket.vault_backend.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.vault.email}"
 }
