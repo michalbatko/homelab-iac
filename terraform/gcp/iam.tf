11b377b5dea3ce6b627840a15263b543e2eb703f
michalbatko <mibatko@gmail.com>
2025-08-29 21:46:25 +0200

    feat: Add GitHub OIDC identity provider

--- terraform/gcp/iam.tf
+++ terraform/gcp/iam.tf
@@ -43,3 +43,24 @@ resource "google_iam_workload_identity_pool" "github" {
   project                   = google_project.homelab_iac.project_id
   workload_identity_pool_id = "github"
 }
+
+resource "google_iam_workload_identity_pool_provider" "github" {
+  project                            = google_project.homelab_iac.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github"
+  display_name                       = "GitHub"
+  description                        = "GitHub Actions identity pool provider"
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.aud"        = "assertion.aud"
+    "attribute.repository" = "assertion.repository"
+  }
+
+  attribute_condition = "attribute.repository == 'michalbatko/homelab-iac'"
+}
